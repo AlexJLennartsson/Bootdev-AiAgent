a6ecffd2515e2b0df37cf7a0456bbe274c210fae
AlexJLennartsson <alexlennartsson@gmail.com>
2025-06-26 23:04:32 +0100

    Added verbose option functionality

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ if(len(sys.argv) < 2):
   print("No prompt was provided, exiting program.")
   exit(1)
 user_prompt = sys.argv[1]
+verbose = False
+if(len(sys.argv) > 2 and sys.argv[2] == "--verbose"):
+  verbose = True
 
 messages = [
   types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -18,5 +21,9 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
+if(verbose):
+  print(f"User prompt: {response.text}Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+else:
+  print(response.text)
+  
