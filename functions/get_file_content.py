54a05c6787475af0c133d7a6c0c3941ab0955e1f
AlexJLennartsson <alexlennartsson@gmail.com>
2025-10-20 23:09:46 +0100

    Cleaned up the initial code for get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,14 +1,16 @@
 import os
+from functions.config import FILE_CHARACTER_LIMIT
 
 def get_file_content(working_directory, file_path):
-  working_directory_full = os.path.abspath(working_directory)
-  file_path_full = os.path.abspath(os.path.join(working_directory, file_path))
-  working_directory_list = []
-  for item in os.listdir(working_directory_full):
-    working_directory_list.append(os.path.abspath(os.path.join(working_directory, item)))
+  working_directory_full = os.path.realpath(working_directory)
+  file_path_full = os.path.realpath(os.path.join(working_directory, file_path))
   
-  print(os.path.abspath(file_path))
-  if(file_path not in os.listdir(working_directory_full)):
+  if(working_directory_full not in file_path_full):
     return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
   if(not os.path.isfile(file_path_full)):
     return f'Error: File not found or is not a regular file: "{file_path}"'
+
+  file = open(file_path_full, 'r')
+  if(len(file) > FILE_CHARACTER_LIMIT):
+    file = file[0:10001] + f'...File "{file_path}" truncated at {FILE_CHARACTER_LIMIT} characters'
+  print(file)
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 
-print(get_file_content("calculator", "../main.py"))
+print(get_file_content("calculator", "mainy.py"))
