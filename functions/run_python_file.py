eb4f1f8d0071f99efe2a09ec49b89834f13322e2
AlexJLennartsson <alexlennartsson@gmail.com>
2025-11-21 18:48:21 +0000

    Completed run_python_file.py function

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -4,7 +4,7 @@ import subprocess
 def run_python_file(working_directory, file_path, args=[]): 
   try:
     working_directory_full = os.path.realpath(working_directory)
-    file_path_full = os.path.realpath(file_path)
+    file_path_full = os.path.realpath(os.path.join(working_directory_full, file_path))
     if(working_directory_full not in file_path_full):
       return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     if(not os.path.isfile(file_path_full)):
@@ -12,7 +12,13 @@ def run_python_file(working_directory, file_path, args=[]):
     if(file_path[-3:] != ".py"):
       return f'Error: "{file_path}" is not a Python file.'
 
-    subprocess.run(args=args, capture_output=True, timeout=30, env=)
+    args = ["python", file_path] + args
+    completed_process = subprocess.run(args=args, capture_output=True, timeout=30, cwd=working_directory_full, text=True)
+    if(completed_process.stdout == ""):
+      return 'No output produced.'
+    if(completed_process.returncode != 0):
+      return f'STDOUT: \n{completed_process.stdout}\nSTDERR: \n{completed_process.stderr}\nProcess exited with code {completed_process.returncode}'
+    return f'STDOUT: \n{completed_process.stdout}\nSTDERR: \n{completed_process.stderr}'
 
   except Exception as e:
     return f'Error: {e}'
--- tests.py
+++ tests.py
@@ -4,3 +4,8 @@ from functions.write_file import write_file
 from functions.run_python_file import run_python_file
 
 print(run_python_file("calculator", "main.py"))
+print(run_python_file("calculator", "main.py", ["3 + 5"]))
+print(run_python_file("calculator", "tests.py"))
+print(run_python_file("calculator", "../main.py"))
+print(run_python_file("calculator", "nonexistent.py"))
+print(run_python_file("calculator", "lorem.txt"))
